f331f01538f75592d9d2dd2f69578f90e6dce3a3
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2021-04-04 21:15:19 -0300

    Configuração de rotas e arquivos para template engine EJS

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const express = require("express")
 const server = express()
 const routes = require('./routes')
 
+server.set('view engine', 'ejs')
+
 // habilita arquivos estáticos
 server.use(express.static("public"))
 
