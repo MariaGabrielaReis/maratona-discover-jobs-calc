3352aa9ae22f2fc8833013ac03b4d6cebed25a64
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2021-04-04 21:44:07 -0300

    Uso de variáveis da EJS para exibir dados na tela de perfil

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,18 @@ const routes = express.Router()
 
 const views = __dirname + '/views/'
 
+const profile = {
+    name: "Maby",
+    avatar: "https://unavatar.now.sh/github/MariaGabrielaReis",
+    "monthly-budget": 3000,
+    "days-per-week": 5,
+    "hours-per-day": 4,
+    "vacation-per-year": 10
+}
+
 routes.get('/', (req, res) => res.render(views + 'index'))
 routes.get('/job', (req, res) => res.render(views + 'job'))
 routes.get('/job/edit', (req, res) => res.render(views + 'job-edit'))
-routes.get('/profile', (req, res) => res.render(views + 'profile'))
+routes.get('/profile', (req, res) => res.render(views + 'profile', { profile }))
 
 module.exports = routes;
