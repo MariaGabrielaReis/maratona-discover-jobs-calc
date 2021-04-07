49c0800d440fe0118de7cbcf577a326e72d5c141
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2021-04-07 10:51:21 -0300

    Criação de serviço para cálculo do orçamento do job

--- src/routes.js
+++ src/routes.js
@@ -72,7 +72,7 @@ const Job = {
                   ...job,
                  remaining,
                   status,
-                  budget: Profile.data['value-hour'] * job['total-hours']
+                  budget: Job.services.calculateBudget(job, Profile.data["value-hour"])
                 }
             })
             
@@ -97,6 +97,20 @@ const Job = {
             })
 
             return res.redirect('/')
+        },
+
+        show(req, res) {
+
+            const jobId = req.params.id
+
+            const job = Job.data.find(job => Number(job.id) === Number(jobId))
+
+            if(!job) {
+                return res.send('Projeto não encontrado!')
+            }
+
+            job.budget = Job.services.calculateBudget(job, Profile.data["value-hour"])
+            return res.render(views + 'job-edit', { job })
         }
     },
 
@@ -119,14 +133,16 @@ const Job = {
             const dayDiff = Math.floor(timeDiffInMs / dayInMs)
         
             return dayDiff
-        }
+        },
+
+        calculateBudget: (job, valueHour) =>  valueHour * job['total-hours']
 
     }
 }
 
 routes.get('/', Job.controllers.index)
 routes.get('/job', Job.controllers.create)
-routes.get('/job/edit', (req, res) => res.render(views + 'job-edit'))
+routes.get('/job/:id', Job.controllers.show)
 routes.get('/profile',  Profile.controllers.index)
 
 routes.post('/profile',  Profile.controllers.update)
