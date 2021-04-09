43b9775a141b75ee3fc868dd22ab3601aa26437f
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2021-04-09 16:05:04 -0300

    Uso do bando de dados para buscar os dados do perfil

--- src/model/Profile.js
+++ src/model/Profile.js
@@ -1,16 +1,22 @@
-let data = {
-  name: "Maby",
-  avatar: "https://unavatar.now.sh/github/MariaGabrielaReis",
-  "monthly-budget": 3000,
-  "days-per-week": 5,
-  "hours-per-day": 4,
-  "vacation-per-year": 10,
-  "value-hour": 75,
-}
+const Database = require('../db/config')
 
 module.exports = {
-  get(){
-    return data
+  async get(){
+    const db = await Database()
+
+    const data = await db.get(`SELECT * FROM profile`)
+
+    await db.close()
+
+    return {
+      name: data.name,
+      avatar: data.avatar,
+      "monthly-budget": data.monthly_budget,
+      "days-per-week": data.days_per_week,
+      "hours-per-day": data.hours_per_day,
+      "vacation-per-year": data.vacation_per_year,
+      "value-hour": data.value_hour
+    }
   },
 
   update(newData){
