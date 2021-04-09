2d5fdc92375dcc999b7f1c51458a8f2d8f0538a9
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2021-04-09 15:49:56 -0300

    Ajustes na criação das tabelas (uso de async e await)

--- src/db/init.js
+++ src/db/init.js
@@ -1,56 +1,64 @@
-const Database = require('config')
+const Database = require('./config')
 
-// conexão com banco - open()
-Database()
+const initDb = {
+    async init() {
+        // conexão com banco
+        const db = await Database()
 
-Database.exec(`CREATE TABLE profile (
-    id INT PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    avatar TEXT,
-    monthly-budget INT,
-    days-per-week INT,
-    hours-per-day INT,
-    vacation-per-year INT,
-    value-hour INT
-);`)
+        //Criando tabelas
+        await db.exec(`CREATE TABLE profile (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            avatar TEXT,
+            monthly_budget INT,
+            days_per_week INT,
+            hours_per_day INT,
+            vacation_per_year INT,
+            value_hour INT
+        );`)
 
-Database.exec(`CREATE TABLE job (
-    id INT PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    daily-hours INT,
-    total-hours INT,
-    created_at DATETIME
-);`)
+        await db.exec(`CREATE TABLE job (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            daily_hours INT,
+            total_hours INT,
+            created_at DATETIME
+        );`)
 
-Database.run(`INSERT INTO profile (
-    name, 
-    avatar,
-    monthly-budget,
-    days-per-week,
-    hours-per-day,
-    vacation-per-year,
-    value-hour
-) VALUES (
-    "Maby",
-    "https://unavatar.now.sh/github/MariaGabrielaReis",
-    3000,
-    5,
-    4,
-    10,
-    75
-);`)
+        //Populando tabelas (inicialmente)
+        await db.run(`INSERT INTO profile (
+            name, 
+            avatar,
+            monthly_budget,
+            days_per_week,
+            hours_per_day,
+            vacation_per_year,
+            value_hour
+        ) VALUES (
+            "Maby",
+            "https://unavatar.now.sh/github/MariaGabrielaReis",
+            3000,
+            5,
+            4,
+            10,
+            75
+        );`)
 
-Database.run(`INSERT INTO profile (
-    name,
-    daily-hours,
-    total-hours,
-    created_at
-) VALUES (
-    "Padaria Pãozinho Quente",
-    2,
-    1,
-    1617992743828
-);`)
+        await db.run(`INSERT INTO job (
+            name,
+            daily_hours,
+            total_hours,
+            created_at
+        ) VALUES (
+            "Padaria Pãozinho Quente",
+            2,
+            1,
+            1617992743828
+        );`)
 
-// fechando conexão com banco
-Database.close()
+        // fechando conexão com banco
+        await db.close()
+    }
+}
+
+initDb.init()
